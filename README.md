831c2807611aa002a82ab653e12477efe11bca16
Linfeng Qian <thewawar@gmail.com>
2020-12-10 14:13:42 +0800

    Add call kind to args

--- README.md
+++ README.md
@@ -18,29 +18,33 @@ FLAGS:
     -V, --version        Prints version information
 
 OPTIONS:
-        --depth <depth>              The call depth [default: 0]
+    -k, --call-kind <call-kind>      The call kind [default: call]  [possible values: call, delegatecall, callcode,
+                                     create, create2]
+    -d, --depth <depth>              The call depth [default: 0]
         --input-data <input-data>    The input data for EVM message. [default: 0x]
         --tx-origin <tx-origin>      The transaction original sender [default:
                                      0x0000000000000000000000000000000000000000]
-        --value <value>              The amount of Ether transferred with the message. [default:
+    -v, --value <value>              The amount of Ether transferred with the message. [default:
                                      0x0000000000000000000000000000000000000000000000000000000000000000]
 ```
 
 Frame structure (`depth = 0`)
 ```
 args[0..2]               => depth
-args[2..3]               => flags
-args[3..35]              => value
-args[35..39]             => input_size
-args[39..39+input_size]  => input_data
+args[2..3]               => call_kind
+args[3..4]               => flags
+args[4..36]              => value
+args[36..40]             => input_size
+args[40..40+input_size]  => input_data
 ```
 
 Frame structure (`depth > 0`)
 ```
 args[0..2]               => depth
 args[2..22]              => tx_origin
-args[22..23]             => flags
-args[23..55]             => value
-args[55..59]             => input_size
-args[59..59+input_size]  => input_data
+args[22..23]             => call_kind
+args[23..24]             => flags
+args[24..56]             => value
+args[56..60]             => input_size
+args[60..60+input_size]  => input_data
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,45 @@
-
 use clap::{App, Arg};
+use numext_fixed_hash::{H160, H256};
 use numext_fixed_uint::U256;
-use numext_fixed_hash::{H256, H160};
 
 fn main() {
     let matches = App::new("polyjuice evm message builder")
         .arg(
             Arg::with_name("depth")
                 .long("depth")
+                .short("d")
                 .takes_value(true)
                 .default_value("0")
-                .help("The call depth")
+                .help("The call depth"),
         )
         .arg(
             Arg::with_name("tx-origin")
                 .long("tx-origin")
                 .takes_value(true)
                 .default_value("0x0000000000000000000000000000000000000000")
-                .help("The transaction original sender")
+                .help("The transaction original sender"),
+        )
+        .arg(
+            Arg::with_name("call-kind")
+                .long("call-kind")
+                .short("k")
+                .takes_value(true)
+                .default_value("call")
+                .possible_values(&["call", "delegatecall", "callcode", "create", "create2"])
+                .help("The call kind"),
         )
         .arg(
             Arg::with_name("static-call")
                 .long("static-call")
-                .help("The flag for EVM message")
+                .help("The flag for EVM message"),
         )
         .arg(
             Arg::with_name("value")
                 .long("value")
+                .short("v")
                 .takes_value(true)
                 .default_value("0x0000000000000000000000000000000000000000000000000000000000000000")
-                .help("The amount of Ether transferred with the message.")
+                .help("The amount of Ether transferred with the message."),
         )
         .arg(
             Arg::with_name("input-data")
@@ -37,7 +47,7 @@ fn main() {
                 .takes_value(true)
                 .required(true)
                 .default_value("0x")
-                .help("The input data for EVM message.")
+                .help("The input data for EVM message."),
         )
         .get_matches();
 
@@ -47,19 +57,42 @@ fn main() {
     } else {
         None
     };
-    let flags: u8 = if matches.is_present("static-call") { 1 } else { 0 };
-    let value_data: H256 = H256::from_hex_str(remove_hex_prefix(matches.value_of("value").unwrap())).unwrap();
+    let kind: u8 = match matches.value_of("call-kind").unwrap() {
+        "call" => 0,
+        "delegatecall" => 1,
+        "callcode" => 2,
+        "create" => 3,
+        "create2" => 4,
+        _ => unreachable!(),
+    };
+    let flags: u8 = if matches.is_present("static-call") {
+        1
+    } else {
+        0
+    };
+    let value_data: H256 =
+        H256::from_hex_str(remove_hex_prefix(matches.value_of("value").unwrap())).unwrap();
     let value: U256 = U256::from_be_bytes(value_data.as_fixed_bytes());
     let input_str = matches.value_of("input-data").unwrap();
     let input_data: Vec<u8> = hex::decode(remove_hex_prefix(input_str)).unwrap();
 
-
-    println!("0x{}", make_bytes(depth, tx_origin_opt.as_ref(), flags, &value, &input_data[..]));
+    println!(
+        "0x{}",
+        make_bytes(
+            depth,
+            tx_origin_opt.as_ref(),
+            kind,
+            flags,
+            &value,
+            &input_data[..]
+        )
+    );
 }
 
 fn make_bytes(
     depth: u16,
     tx_origin_opt: Option<&H160>,
+    kind: u8,
     flags: u8,
     value: &U256,
     input_data: &[u8],
@@ -69,6 +102,7 @@ fn make_bytes(
     if let Some(tx_origin) = tx_origin_opt {
         buf.extend(tx_origin.as_bytes());
     }
+    buf.push(kind);
     buf.push(flags);
     buf.extend(&value.to_le_bytes());
     buf.extend(&(input_data.len() as u32).to_le_bytes());
@@ -83,23 +117,21 @@ fn remove_hex_prefix(mut input_str: &str) -> &str {
     input_str
 }
 
-
 #[cfg(test)]
 mod test {
 
     use super::*;
+    use numext_fixed_hash::h160;
     use numext_fixed_uint::u256;
-    use numext_fixed_hash::{h256, h160};
     #[test]
     fn test_make_bytes() {
-
         assert_eq!(
-            make_bytes(0, None, 0, &u256!("0x0"), &[][..]),
-            "000000000000000000000000000000000000000000000000000000000000000000000000000000",
+            make_bytes(0, None, 3, 0, &u256!("0x0"), &[][..]),
+            "00000300000000000000000000000000000000000000000000000000000000000000000000000000",
         );
         assert_eq!(
-            make_bytes(1, Some(&h160!("0x3300000000000000000000000000000000003333")), 1, &u256!("0x0"), &[0x33][..]),
-            "010033000000000000000000000000000000000033330100000000000000000000000000000000000000000000000000000000000000000100000033",
+            make_bytes(1, Some(&h160!("0x3300000000000000000000000000000000003333")), 0, 1, &u256!("0x0"), &[0x33][..]),
+            "01003300000000000000000000000000000000003333000100000000000000000000000000000000000000000000000000000000000000000100000033",
         );
     }
 }
